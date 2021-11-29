65b97d240313341e0c0799d2f3917caa1c495c10
I-make-gamez <ebelcher143@gmail.com>
2021-11-29 02:59:49 +0000

    0.(-3).0

--- java.js
+++ java.js
@@ -1,35 +1,113 @@
 const doge = document.querySelector('.doge');
 const u1 = document.querySelector('.u1');
+const u2 = document.querySelector('.u2');
 const cpwr = document.querySelector('#cpwr');
-const dc = document.querySelector('#dc');
+const cps = document.querySelector('#cps');
+const dc = document.querySelector('.dc');
+const version = document.querySelector('.vers');
 
+var ver = '0.2.2';
+var page = window;
+const ask = page.prompt;
+
+var totalDc = readCookie('totalDc');
+var clickpwr = readCookie('clickpower');
 var doco = 0;
-var clipo = 1
+var clipo = 1;
+var clipes = 0;
+var cpsok = 0;
 
-function updateDC(){
-    dc.innerHTML = `DogeCoin: ${doco}`;
+function loadVersion() {
+    version.innerHTML = `Version: ${ver}`
 };
 
-function loadData(){
-    doco = parseInt(localStorage.getItem('dogco'));
-    clipo = parseInt(localStorage.getItem('cpowr'));
-    dc.innerHTML = `DogeCoin: ${doco}`;
-    cpwr.innerHTML = `Clickpower: ${clipo}`;
-
+function cpsGo() {
+    setInterval(function () {
+        doco += clipes;
+        dc.innerHTML = `DogeCoin: ${doco}`;
+    }, 1000)
 };
 
-doge.addEventListener('click', function(){
+function loadData() {
+    setTimeout(function(){
+        doco = readCookie('totalDc');
+        dc.innerHTML = `DogeCoin: ${doco}`;
+    }, 100);
+    setTimeout(function(){
+        clipo = parseInt(readCookie('clickpower'));
+        cpwr.innerHTML = `Clickpower: ${clipo}`
+    }, 100)
+};
+
+function deleteData() {
+    let ans = ask("Are you sure?\n[Y|N]")
+    switch (ans) {
+        case 'y' || 'Y':
+            doco = 0;
+            dc.innerHTML = `DogeCoin: ${doco}`;
+            eraseCookie('totalDc');
+            clipo = 1;
+            cpwr.innerHTML = `Clickpower: ${clipo}`
+            eraseCookie('clickpower');
+            break;
+        case 'n' || 'N':
+            doco = doco
+            clipo = clipo
+    };
+};
+
+doge.addEventListener('click', function () {
     doco += clipo;
-    updateDC();
-    localStorage.setItem('dogco', doco);
+    dc.innerHTML = `DogeCoin: ${doco}`;
+    if(totalDc){
+        eraseCookie('totalDc');
+        createCookie('totalDc', doco, 1000)
+    }else{
+        return;
+    };
 });
 
-u1.addEventListener('click', function(){
-    if(doco >= 100){
+u1.addEventListener('click', function () {
+    if (doco >= 100) {
         doco -= 100
-        updateDC();
+        dc.innerHTML = `DogeCoin: ${doco}`;
         clipo += 1;
-        localStorage.setItem('cpowr', clipo);
         cpwr.innerHTML = `Clickpower: ${clipo}`
+        if(clickpwr){
+            eraseCookie('clickpower');
+            createCookie('clickpower', clipo, 1000);
+        }else{
+            return;
+        };
     }
 });
+
+u2.addEventListener('click', function () {
+    if (doco >= 2000) {
+
+        doco -= 2000;
+        dc.innerHTML = `DogeCoin: ${doco}`;
+        clipes += 1;
+        cps.innerHTML = `Clicks Per Second: ${clipes}`;
+        cpsok = 1;
+        cpsGo();
+    }
+});
+
+document.onkeydown = function (e) {
+    var e = e || page.event;
+    if (e.ctrlKey && e.altKey && e.key === 'm') {
+        let ans = ask(`Hello, You've reached the admin menu.\nEnter a number [1-1]`)
+        switch (ans) {
+            case ans = '1':
+                let total = ask(`Enter DogeCoin Needed.`)
+                doco += parseInt(total);
+                dc.innerHTML = `DogeCoin: ${doco}`;
+
+                break;
+            case ans = '2':
+                alert("You've Found The Secret!");
+                break;
+        }
+    }
+};
